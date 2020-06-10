b209624a5cf4d09ad87331ad09f3fe1b885cb3d1
bolu3 <coutodiego.f@gmail.com>
2020-06-10 11:41:58 -0300

    Clases de transacciones, cuenta, init

---
+++ Transacciones.py
@@ -0,0 +1,15 @@
+class Transaccion:
+
+    def __init__(self, categoria, descripcion, monto, fecha):
+        self.categoria = categoria
+        self.descripcion = descripcion
+        self.monto = monto
+        self.fecha = fecha
+
+
+
+class Gasto(Transaccion):
+    pass
+
+class Ingreso(Transaccion):
+    pass
