9ac15228a6651941162a224101c15d4115497513
bolu3 <coutodiego.f@gmail.com>
2020-06-11 00:38:18 -0300

    Avance en app

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from account import Account
 from menus import mainMenu
 
+accountList = []
+
 
 def ppal():
-    accountList = []
+
+    
     a1 = Account("Diego")
     accountList.append(a1)
     a2 = Account("Empresa")
     accountList.append(a2)
-
-    for x in accountList:
-        print(x.name)
-        print(x.ahorro)
-
+    
+    
     mainMenu()
+    
 
 
+
+        
+        
 ppal()
